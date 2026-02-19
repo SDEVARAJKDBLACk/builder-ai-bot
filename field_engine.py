ce4e8acfd1eae0066233b9226a50a3e88b2b94c3
DEVARAJ S <149044460+SDEVARAJKDBLACk@users.noreply.github.com>
2026-02-19 17:44:09 +0530

    ai_ engine.py

--- field_engine.py
+++
@@ -1,7 +0,0 @@
-
-def core_fields():
-    return [
-        "name","age","gender","email","phone","address","city","state","country",
-        "pincode","company","designation","salary","id","date","amount","account",
-        "transaction","category","description"
-    ]
