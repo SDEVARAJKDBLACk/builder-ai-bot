07fcbd2ca93e270b18b53efb44a5f6b697ac9696
DEVARAJ S <149044460+SDEVARAJKDBLACk@users.noreply.github.com>
2026-02-20 14:13:34 +0530

    Delete excel_export.py

--- excel_export.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datetime import datetime
-
-def export_excel(data):
-    df = pd.DataFrame(data)
-    name = f"ai_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
-    df.to_excel(name,index=False)
